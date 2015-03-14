8d04c172978d9b92433214c59a04c90a3a8c7de4
javacreed <albert@javacreed.com>
2015-03-14 17:05:16 +0100

    Initial Import

---
+++ src/main/java/com/javacreed/examples/di/part2/FastEmailService.java
@@ -0,0 +1,30 @@
+/*
+ * #%L
+ * Why should we use dependency injection?
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 - 2015 Java Creed
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.javacreed.examples.di.part2;
+
+public class FastEmailService implements MessageService {
+
+  public void sendMessage(final String subject, final String message) {
+    System.out.printf("Fast Email: %s, %s%n ", subject, message);
+  }
+
+}
---
+++ src/main/java/com/javacreed/examples/di/part4/Person.java
@@ -0,0 +1,45 @@
+/*
+ * #%L
+ * Why should we use dependency injection?
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 - 2015 Java Creed
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.javacreed.examples.di.part4;
+
+import com.google.inject.Inject;
+
+public class Person {
+
+  private final MessageService messageService;
+  private final WritingService writingService;
+
+  @Inject
+  private Person(final MessageService messageService, WritingService writeService) {
+    this.messageService = messageService;
+    this.writingService = writeService;
+  }
+
+  public void greetFriend() {
+    messageService.sendMessage("Hello", "Hello my friend :)");
+  }
+  
+  public void writeToFriend() {
+    writingService.write("Hello my friend :)");
+  }
+
+}
